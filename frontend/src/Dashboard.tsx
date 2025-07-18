a3c7d744dadaccae3bfbd77cbf6b06db712e8ba3
yufenlai0825 <yufenlai0825@gmail.com>
2025-07-18 13:27:29 +0200

    create css files, import

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Dashboard.css";
 
 interface DashboardProps {
   results: any;
--- frontend/src/research.tsx
+++ frontend/src/research.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./research.css";
 
 interface ResearchFormProps {
   onSubmit: (query: string, depth: number, breadth: number) => void;
